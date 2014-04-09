78f5a0174b222ff5e1decec6d10d0b2605795e68
Philipp Sackl <philipp@mozilla.com>
2014-04-09 15:47:03 +0200

    initial commit

---
+++ js/app.js
@@ -0,0 +1,6 @@
+var frontImage = document.getElementById('front-image');
+
+// Slide out the front image
+frontImage.addEventListener('click', function(e) {
+  frontImage.classList.toggle('out');
+});
