ea26d66e6600cf8d19a25b4022fb4f4ac84e81cf
Philipp Sackl <philipp@mozilla.com>
2014-04-10 15:53:56 +0200

    too many things to list them all

--- js/app.js
+++ js/app.js
@@ -4,3 +4,16 @@ var frontImage = document.getElementById('front-image');
 frontImage.addEventListener('click', function(e) {
   frontImage.classList.toggle('out');
 });
+
+// Showing the label for toolbar buttons
+var labelPlaceholder = document.getElementById('label-placeholder');
+var toolbarButtons = document.querySelectorAll('.toolbar-button');
+for (var i=0; i<toolbarButtons.length; i++) {
+  toolbarButtons[i].addEventListener('mouseover', function(e) {
+    var label = e.currentTarget.getAttribute('data-label');
+    labelPlaceholder.innerHTML = label;
+  });
+  toolbarButtons[i].addEventListener('mouseout', function(e) {
+    labelPlaceholder.innerHTML = "";
+  });
+}
