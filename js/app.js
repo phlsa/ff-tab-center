7bedbe940526f60bd4516125673fe6f2ccef40b3
Philipp Sackl <philipp@mozilla.com>
2014-04-11 15:35:43 +0200

    add search field and open button

--- js/app.js
+++ js/app.js
@@ -1,8 +1,13 @@
 var frontImage = document.getElementById('front-image');
+var trigger = document.getElementById('trigger');
 
 // Slide out the front image
-frontImage.addEventListener('click', function(e) {
+trigger.addEventListener('click', function(e) {
+  e.stopPropagation();
   frontImage.classList.toggle('out');
+});
+frontImage.addEventListener('click', function(e) {
+  frontImage.classList.remove('out');
 });
 
 // Showing the label for toolbar buttons
